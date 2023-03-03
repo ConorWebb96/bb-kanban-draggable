7563a9921fe78164fb0ad4f5420bf5f28083ff8f
andz-bb <andrew.thompson@budibase.com>
2023-03-03 00:42:50 +0000

    messy commit with spectrum theming and some fixes

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,38 +1,56 @@
 <script>
   import { getContext, onMount } from "svelte";
-  import KabanColumns from "./components/columns.svelte";
+  import KabanColumns from "./components/Columns.svelte";
+  import {
+    Input,
+    Label,
+    Layout,
+    ModalContent,
+    Modal,
+    Skeleton,
+  } from "@budibase/bbui";
+
   // variables
   export let dataProvider;
   export let table;
   export let kabanCardTitles;
   export let onClick;
+  export let title = "";
+
   let tableStatuses = [];
   let columns = {};
   let droppable;
   let columnsLoaded = false;
   let kabanColumns;
-  const cardsTableId = dataProvider.datasource.tableId;
-  const statusTableId = table.tableId;
+  let modal;
+  let newColumnTitle;
+
+  const cardsTableId = dataProvider?.datasource.tableId;
+  const statusTableId = table?.tableId;
 
   const { styleable, API, notificationStore } = getContext("sdk");
   const component = getContext("component");
   // fetch tables function
   function fetchTables() {
-    API.fetchTableData(statusTableId).then(function(data) { // get the promised statusTable data
-      tableStatuses = data; // set table status after looping of data
-      data.forEach((status) => {
-        columns[status.Name] = [];
+    API.fetchTableData(statusTableId)
+      .then(function (data) {
+        // get the promised statusTable data
+        tableStatuses = data; // set table status after looping of data
+        data.forEach((status) => {
+          columns[status.Name] = [];
+        });
+        dataProvider.rows.forEach((card) => {
+          const state = card[kabanCardTitles][0].primaryDisplay;
+          columns[state].push(card);
+        });
       })
-      dataProvider.rows.forEach((card) => {
-        const state = card[kabanCardTitles][0].primaryDisplay;
-        columns[state].push(card);
+      .catch((err) => {
+        console.log(err); // logging the error to the console.
+      })
+      .finally(function () {
+        columnsLoaded = true; // set columns loaded to true after this opperation has been completed
       });
-    }).catch(err => {
-        console.log(err) // logging the error to the console.
-    }).finally(function() {
-      columnsLoaded = true; // set columns loaded to true after this opperation has been completed
-    });
-  };
+  }
   // on page load
   onMount(() => {
     // define a Promise that resolves when the variable is populated
@@ -49,88 +67,118 @@
       });
     }
     // wait for the variable to become populated, then run the process
-    waitForVariable(dataProvider).then(function () {
-      fetchTables();
-    }).catch(function (err) {
-      console.log(err); // logging the error to the console.
-    });
+    waitForVariable(dataProvider)
+      .then(function () {
+        fetchTables();
+      })
+      .catch(function (err) {
+        console.log(err); // logging the error to the console.
+      });
   });
   // add new column
   async function addColumn() {
-    const newColumnTitle = prompt("Enter column title");
-    if (newColumnTitle && !(newColumnTitle in columns)) { // check if column already exists, if it does don't let them create.
+    if (newColumnTitle && !(newColumnTitle in columns)) {
+      // check if column already exists, if it does don't let them create.
       const newColumnObject = [];
       const newColumns = { [newColumnTitle]: newColumnObject, ...columns };
       try {
-        await API.saveRow({ // create a new column on the backend 
+        await API.saveRow({
+          // create a new column on the backend
           tableId: statusTableId,
-          Name: newColumnTitle
+          Name: newColumnTitle,
         });
         columns = newColumns;
         fetchTables(); // reload main data after creating new column row
-      } catch(error) {
+      } catch (error) {
         console.log(error);
       }
-      notificationStore.actions.success(`New column: ${newColumnTitle} has been successfully created!`);
-    }else {
-      notificationStore.actions.error("Sorry you have either tried to add a row without a name, or tried to create a duplicate column.");
+      notificationStore.actions.success(
+        `New column: ${newColumnTitle} has been successfully created!`
+      );
+    } else {
+      notificationStore.actions.error(
+        "Sorry you have either tried to add a row without a name, or tried to create a duplicate column."
+      );
     }
   }
   // call the moveItem() function in the child component
   function handleDrop(event) {
-    childComponent.moveItem(); 
+    childComponent.moveItem();
   }
 </script>
 
 <div use:styleable={$component.styles} class="container">
-    {#if columnsLoaded}
-      <div class="flexed space-x-5">
-          <span stlye="display: block;">You have a total of {Object.keys(columns).length} columns</span>
-          <button on:click={addColumn} class="ml-5">Add Column</button>
+  {#if !dataProvider || !table}
+    <div class="placeholder">
+      Please provide a Data Provider and a Relationship Table
+    </div>
+  {:else if columnsLoaded}
+    <div class="header">
+      <h2>{title}</h2>
+      <div>
+        <button
+          on:click={modal.show()}
+          class="spectrum-Button spectrum-Button--sizeM spectrum-Button--cta"
+          >Add Column</button
+        >
       </div>
-      <div class="kanban-board" on:drop|preventDefault={() => handleDrop} bind:this={droppable}>
-          <KabanColumns 
-              {columns} 
-              {droppable} 
-              {tableStatuses}
-              {kabanCardTitles}
-              {onClick}
-              {cardsTableId}
-              {fetchTables}
-              bind:this={kabanColumns}
-          />
-      </div>
-    {:else}
-      <p>Sorry but your content hasn't been loaded</p>
-    {/if}
+    </div>
+    <div
+      class="kanban-board"
+      on:drop|preventDefault={() => handleDrop}
+      bind:this={droppable}
+    >
+      <KabanColumns
+        {columns}
+        {droppable}
+        {tableStatuses}
+        {kabanCardTitles}
+        {onClick}
+        {cardsTableId}
+        {fetchTables}
+        bind:this={kabanColumns}
+      />
+    </div>
+    <p>You have a total of {Object.keys(columns).length} columns</p>
+
+    <Modal bind:this={modal}>
+      <ModalContent title="Add Column" size="XL" onConfirm={addColumn}>
+        <form>
+          <Layout noPadding gap="S">
+            <div class="form-row">
+              <Label>Column name</Label>
+              <Input on:change bind:value={newColumnTitle} />
+            </div>
+          </Layout>
+        </form>
+      </ModalContent>
+    </Modal>
+  {:else}
+    <Skeleton />
+  {/if}
 </div>
 
 <style>
-  .flexed {
-    display: flex;
-    align-items: center;
-  }
   *:focus {
     outline: none;
   }
   .kanban-board {
     display: flex;
     overflow: scroll;
-    -ms-overflow-style: none;  /* IE and Edge */
-    scrollbar-width: none;  /* Firefox */
+    -ms-overflow-style: none; /* IE and Edge */
+    scrollbar-width: none; /* Firefox */
     width: 100%;
   }
   .kanban-board::-webkit-scrollbar {
     display: none;
   }
-  button {
-    max-width: fit-content!important;
-    margin: 0.5rem 0;
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0.5rem;
   }
-  button {
-    cursor: pointer;
-  }
-  .ml-5 {
-    margin-left: 1rem;
+  .placeholder {
+    color: var(--spectrum-global-color-gray-600);
   }
 </style>
--- src/components/columns.svelte
+++ src/components/columns.svelte
@@ -1,219 +1,288 @@
 <script>
-    import { getContext } from "svelte"
-    import Card from "./card.svelte";
+  import { getContext } from "svelte";
+  import Card from "./Card.svelte";
 
-    // variables
-    export let columns;
-    export let droppedArrayName;
-    export let droppable;
-    export let tableStatuses;
-    export let kabanCardTitles;
-    export let onClick;
-    export let cardsTableId;
-    export let fetchTables;
-    let draggedItem;
-    const component = getContext("component");
-    const { API, notificationStore } = getContext("sdk");
-    const reducedStatuses = tableStatuses.map(({ _id, Name, tableId }) => ({ _id, Name, tableId })); // reduce statuses the array for more mangable data
-    // set emitted item from card component
-    function handleDragStart(event) {
-        draggedItem = event.detail;
+  // variables
+  export let columns;
+  export let draggedArrayName;
+  export let droppable;
+  export let tableStatuses;
+  export let kabanCardTitles;
+  export let onClick;
+  export let cardsTableId;
+  export let fetchTables;
+
+  let draggedColumn;
+  let draggedItem;
+  const component = getContext("component");
+  const { API, notificationStore } = getContext("sdk");
+  const reducedStatuses = tableStatuses.map(({ _id, Name, tableId }) => ({
+    _id,
+    Name,
+    tableId,
+  })); // reduce statuses the array for more mangable data
+  // set emitted item from card component
+  function handleDragStart(event) {
+    draggedItem = event.detail;
+  }
+  // emit to refresh columns
+  function handleColumnsUpdated(event) {
+    columns = event.detail;
+  }
+  // move items kaban style
+  function moveItem() {
+    const findDroppedStatusObj = reducedStatuses.find(
+      (status) => status.Name === draggedArrayName
+    ); // find specific object within reduce statuses arr
+    // delete state field and update with new moved state info
+    delete draggedItem[kabanCardTitles];
+    draggedItem[kabanCardTitles] = {
+      _id: findDroppedStatusObj._id,
+      primaryDisplay: findDroppedStatusObj.Name,
+    };
+    // find the dragged item in the original array
+    const originalArrayName = Object.keys(columns).find((arrayName) =>
+      columns[arrayName].some(
+        (item) => item["Auto ID"] === draggedItem["Auto ID"]
+      )
+    );
+    const originalArray = columns[originalArrayName]; // Get the original column array
+    const draggedIndex = originalArray.findIndex(
+      (item) => item["Auto ID"] === draggedItem["Auto ID"]
+    ); // get the dragged index
+    // check if the dragged item is being dropped onto its original column
+    if (originalArrayName === draggedArrayName) {
+      return; // exit the function without making any changes
     }
-    // emit to refresh columns
-    function handleColumnsUpdated(event) {
-        columns = event.detail;
+    // remove the dragged item from the original array and add it to the dropped array
+    const droppedArray = columns[draggedArrayName];
+    const updatedDraggedItem = { ...draggedItem }; // make a copy of the dragged item to avoid modifying the original
+    originalArray.splice(draggedIndex, 1); // remove dragged item from original Array
+    droppedArray.push(updatedDraggedItem); // push drag item to new dragged array
+    // update the columns object
+    columns = {
+      ...columns,
+      [originalArrayName]: originalArray,
+      [draggedArrayName]: droppedArray,
+    };
+    // save card in the backend after its moved.
+    API.saveRow({
+      _id: draggedItem._id,
+      tableId: draggedItem.tableId,
+      Title: draggedItem.Title,
+      [kabanCardTitles]: [draggedItem[kabanCardTitles]],
+    });
+    notificationStore.actions.success(
+      `Your card ${draggedItem.Title} has been successfully moved!`
+    );
+    draggedArrayName = null;
+  }
+  function deleteColumn(columnName) {
+    const findDeletedColumn = reducedStatuses.find(
+      (status) => status.Name === columnName
+    ); // find specific object within reduce statuses arr
+    // prevent deleting of the Backlog column
+    if (columnName === "Backlog") {
+      return;
     }
-    // move items kaban style
-    function moveItem() {
-        const findDroppedStatusObj = reducedStatuses.find(status => status.Name === droppedArrayName); // find specific object within reduce statuses arr
-        // delete state field and update with new moved state info
-        delete draggedItem[kabanCardTitles];
-        draggedItem[kabanCardTitles] = {
-            _id: findDroppedStatusObj._id,
-            primaryDisplay: findDroppedStatusObj.Name
-        };
-        // find the dragged item in the original array
-        const originalArrayName = Object.keys(columns).find((arrayName) =>
-            columns[arrayName].some((item) => item["Auto ID"] === draggedItem["Auto ID"])
-        );
-        const originalArray = columns[originalArrayName]; // Get the original column array
-        const draggedIndex = originalArray.findIndex((item) => item["Auto ID"] === draggedItem["Auto ID"]); // get the dragged index
-        // check if the dragged item is being dropped onto its original column
-        if (originalArrayName === droppedArrayName) {
-            return; // exit the function without making any changes
-        }
-        // remove the dragged item from the original array and add it to the dropped array
-        const droppedArray = columns[droppedArrayName];
-        const updatedDraggedItem = { ...draggedItem }; // make a copy of the dragged item to avoid modifying the original
-        originalArray.splice(draggedIndex, 1); // remove dragged item from original Array
-        droppedArray.push(updatedDraggedItem); // push drag item to new dragged array
-        // update the columns object
-        columns = {
-            ...columns,
-            [originalArrayName]: originalArray,
-            [droppedArrayName]: droppedArray
-        };
-        // save card in the backend after its moved.
-        API.saveRow({
-            _id: draggedItem._id,
-            tableId: draggedItem.tableId,
-            Title: draggedItem.Title,
-            [kabanCardTitles]: [draggedItem[kabanCardTitles]]
-        });
-        notificationStore.actions.success(`Your card ${draggedItem.Title} has been successfully moved!`);
-    }
-    function deleteColumn(columnName) {
-        const findDeletedColumn = reducedStatuses.find(status => status.Name === columnName); // find specific object within reduce statuses arr
-        // prevent deleting of the Backlog column
-        if (columnName === "Backlog") { 
-            return;
-        }
-        // confirm deletion of column
-        const confirmed = confirm(`Are you sure you want to delete the ${columnName} column?`);
-        if (confirmed) {
-            // move any cards in the specified column to Backlog
-            if (columns[columnName]) {
-                let findBacklogStatus;
-                if (!columns["Backlog"]) {
-                    columns["Backlog"] = [];
-                    // create backlog if it doesn't exist
-                    const backlogTable = API.saveRow({
-                        tableId: findDeletedColumn.tableId,
-                        Name: "Backlog"
-                    });
-                    backlogTable.then((data) => {
-                        // bulk move cards within the backend to backlog
-                        columns["Backlog"].forEach((card) => {
-                            API.saveRow({
-                                _id: card._id,
-                                tableId: card.tableId,
-                                Title: card.Title,
-                                [kabanCardTitles]: [data]
-                            });
-                        })
-                    }).catch((error) => {
-                        console.error(error);
-                    });
-                } else {
-                    findBacklogStatus = reducedStatuses.find(status => status.Name === 'Backlog'); // find backlog data within arr
-                    // bulk move cards within the backend to backlog
-                    columns[columnName].forEach((card) => {
-                        API.saveRow({
-                            _id: card._id,
-                            tableId: card.tableId,
-                            Title: card.Title,
-                            [kabanCardTitles]: [findBacklogStatus]
-                        });
-                    })
-                }
-                // delete column
-                API.deleteRow({
-                    tableId: findDeletedColumn.tableId,
-                    rowId: findDeletedColumn._id,
-                    revId: findDeletedColumn._rev
-                })
-                columns["Backlog"] = columns["Backlog"].concat(columns[columnName]);
-            }
-            // delete the specified column
-            delete columns[columnName];
-        }
-        // Refresh the columns
-        columns = columns; 
-        notificationStore.actions.success(`Your column ${columnName} has been successfully deleted!`);
-    }
-    // add new card to specific column
-    async function addCard(arrayName) {
-        const findAddStatus = reducedStatuses.find(status => status.Name === arrayName); // find specific object within reduce statuses arr
-        try {
-            // save card in the backend after its moved.
-            await API.saveRow({
-                tableId: cardsTableId,
-                Title: "New Card",
-                Description: "",
-                [kabanCardTitles]: [findAddStatus]
+    // confirm deletion of column
+    const confirmed = confirm(
+      `Are you sure you want to delete the ${columnName} column?`
+    );
+    if (confirmed) {
+      // move any cards in the specified column to Backlog
+      if (columns[columnName]) {
+        let findBacklogStatus;
+        if (!columns["Backlog"]) {
+          columns["Backlog"] = [];
+          // create backlog if it doesn't exist
+          const backlogTable = API.saveRow({
+            tableId: findDeletedColumn.tableId,
+            Name: "Backlog",
+          });
+          backlogTable
+            .then((data) => {
+              // bulk move cards within the backend to backlog
+              columns["Backlog"].forEach((card) => {
+                API.saveRow({
+                  _id: card._id,
+                  tableId: card.tableId,
+                  Title: card.Title,
+                  [kabanCardTitles]: [data],
+                });
+              });
+            })
+            .catch((error) => {
+              console.error(error);
             });
-            // Call the fetchData function
-            // await fetchTables();
-            columns[arrayName] = [  
-                ...columns[arrayName],
-                {
-                    Title: "New Card",
-                    Description: "",
-                    [kabanCardTitles]: [findAddStatus]
-                }
-            ];
-            // update current column details
-        }catch(error){
-            console.log(error);
+        } else {
+          findBacklogStatus = reducedStatuses.find(
+            (status) => status.Name === "Backlog"
+          ); // find backlog data within arr
+          // bulk move cards within the backend to backlog
+          columns[columnName].forEach((card) => {
+            API.saveRow({
+              _id: card._id,
+              tableId: card.tableId,
+              Title: card.Title,
+              [kabanCardTitles]: [findBacklogStatus],
+            });
+          });
         }
-        console.log(columns);
-        notificationStore.actions.success(`Your new card has been added to ${arrayName}!`);
+        // delete column
+        API.deleteRow({
+          tableId: findDeletedColumn.tableId,
+          rowId: findDeletedColumn._id,
+          revId: findDeletedColumn._rev,
+        });
+        columns["Backlog"] = columns["Backlog"].concat(columns[columnName]);
+      }
+      // delete the specified column
+      delete columns[columnName];
     }
+    // Refresh the columns
+    columns = columns;
+    notificationStore.actions.success(
+      `Your column ${columnName} has been successfully deleted!`
+    );
+  }
+  // add new card to specific column
+  async function addCard(arrayName) {
+    const findAddStatus = reducedStatuses.find(
+      (status) => status.Name === arrayName
+    ); // find specific object within reduce statuses arr
+    try {
+      // save card in the backend after its moved.
+      await API.saveRow({
+        tableId: cardsTableId,
+        Title: "New Card",
+        Description: "",
+        [kabanCardTitles]: [findAddStatus],
+      });
+      // Call the fetchData function
+      // await fetchTables();
+      columns[arrayName] = [
+        ...columns[arrayName],
+        {
+          Title: "New Card",
+          Description: "",
+          [kabanCardTitles]: [findAddStatus],
+        },
+      ];
+      // update current column details
+    } catch (error) {
+      console.log(error);
+    }
+    console.log(columns);
+    notificationStore.actions.success(
+      `Your new card has been added to ${arrayName}!`
+    );
+  }
+
+  function handleDragover(name) {
+    draggedArrayName = name;
+    droppable.classList.add("droppable");
+  }
+
+  function handleDragleave(name) {
+    draggedArrayName = null;
+    droppable.classList.remove("droppable");
+  }
 </script>
 
 {#each Object.entries(columns) as [arrayName, column] (arrayName)}
-    <div class="kanban-column">
-        <div class="flexed">
-            <h2>{arrayName + ' - ' + column.length}</h2>
-            {#if arrayName != "Backlog"}
-                <button class="delete" on:click={() => deleteColumn(arrayName)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                    </svg>
-                </button>
-            {/if}
+  <div
+    class="spectrum-Dropzone kanban-column"
+    class:is-dragged={draggedArrayName === arrayName}
+    on:dragover|preventDefault={handleDragover(arrayName)}
+    on:dragleave|preventDefault={handleDragleave(arrayName)}
+    on:drop|preventDefault={moveItem}
+    role="region"
+    style="width: 300px;"
+  >
+    <div class="flexed">
+      <h2
+        class="spectrum-Heading spectrum-Heading--sizeM spectrum-Heading--regular spectrum-IllustratedMessage-heading"
+      >
+        {arrayName + " - " + column.length}
+      </h2>
+      <button
+        class="spectrum-ClearButton spectrum-ClearButton--sizeL spectrum-ClearButton--overBackground"
+        disabled={arrayName === "Backlog"}
+        on:click={deleteColumn(arrayName)}
+      >
+        <div class="spectrum-ClearButton-fill">
+          <svg
+            class="spectrum-ClearButton-icon spectrum-Icon spectrum-UIIcon-Cross200"
+            focusable="false"
+            aria-hidden="true"
+          >
+            <use xlink:href="#spectrum-css-icon-Cross200" />
+          </svg>
         </div>
-        <div
-            on:dragover|preventDefault={() => droppable.classList.add("droppable")}
-            on:dragleave|preventDefault={() => droppable.classList.remove("droppable")}
-            on:drop|preventDefault={() => {
-                droppedArrayName = arrayName;
-                moveItem();
-            }}
-            class="h-full"
-        >
-            <Card {columns} {column} {arrayName} {onClick} on:item-dragged={handleDragStart} on:columnsUpdated={handleColumnsUpdated} />
-            <button class="ml-5" on:click={() => addCard(arrayName)}>Add Card</button>
-        </div>
+      </button>
     </div>
+    <div class="flexed">
+      <div class="spectrum-Detail spectrum-Detail--sizeM">
+        {column.length} cards
+      </div>
+      <button
+        class="spectrum-ClearButton spectrum-ClearButton--sizeM spectrum-ClearButton--overBackground"
+        on:click={addCard(arrayName)}
+      >
+        <div class="spectrum-ClearButton-fill">
+          <svg
+            class="spectrum-Icon spectrum-ClearButton-icon spectrum-Icon--sizeM spectrum-icon-18-Add"
+            focusable="false"
+            aria-hidden="true"
+          >
+            <use xlink:href="#spectrum-icon-18-Add" />
+          </svg>
+        </div>
+      </button>
+    </div>
+    <div class="h-full">
+      <Card
+        {columns}
+        {column}
+        {arrayName}
+        {onClick}
+        on:item-dragged={handleDragStart}
+        on:columnsUpdated={handleColumnsUpdated}
+      />
+    </div>
+  </div>
 {/each}
 
 <style>
-    .kanban-column {
-        flex: 1;
-        margin: 0 1rem;
-        background-color: #a4a4a4;
-        padding: 1rem;
-        border-radius: 0.5rem;
-        min-width: 350px;
-        max-width: 350px;
-    }
-    .kanban-column:first-child {
-        margin-left: 0;
-    }
-    .kanban-column:last-child {
-        margin-right: 0;
-    }
-    button {
-        margin: 0.5rem 0;
-    }
-    .flexed {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-    }
-    svg {
-        height: 1rem;
-        width: 1rem;
-    }
-    button {
-        cursor: pointer;
-    }
-    .delete svg {
-        color: red;
-    }
-    .h-full {
-        height: 100%;
-        max-height: 500px;
-    }
+  .kanban-column {
+    flex: 1;
+    margin: 0 0.5rem;
+    /* background-color: #a4a4a4; */
+    padding: 1rem;
+    border-radius: 0.5rem;
+    min-width: 350px;
+    max-width: 350px;
+  }
+  .kanban-column:first-child {
+    margin-left: 0;
+  }
+  .kanban-column:last-child {
+    margin-right: 0;
+  }
+
+  .flexed {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .h-full {
+    height: 100%;
+    max-height: 500px;
+    overflow-y: auto;
+  }
+
+  .spectrum-Dropzone {
+    border-style: solid;
+  }
 </style>
