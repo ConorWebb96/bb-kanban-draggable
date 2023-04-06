09c57bebdd72c9bf2f864dc0af8f5f54997aea86
Conor Webb <conorwebb59@gmail.com>
2023-04-06 10:36:20 +0100

    Altered filtering, only filter cards instead of columns.

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,6 +1,7 @@
 <script>
   import { getContext, onMount } from "svelte";
   import KanbanColumns from "./components/Columns.svelte";
+  import SortColumns from "./components/ColumnsSort.svelte";
   import {
     Input,
     Label,
@@ -23,19 +24,20 @@
   let columnsLoaded = false;
   let kanbanColumns;
   let modal;
+  let orderModal;
   let newColumnTitle;
   let searchQuery = "";
 
   $: filteredColumns = searchQuery.length > 3
-  ? Object.entries(columns).reduce((result, [key, value]) => {
+  ? Object.fromEntries(Object.entries(columns).map(([key, value]) => {
       const filteredCards = value.filter(card => {
         const regex = new RegExp(searchQuery, 'gi');
         const match = (card.Title?.match(regex) || []).length > 0 ||
           (card.Description?.match(regex) || []).length > 0;
         return match;
       });
-      return filteredCards.length ? { ...result, [key]: filteredCards } : result;
-    }, {})
+      return [key, filteredCards];
+    }))
   : columns;
 
   const cardsTableId = dataProvider?.datasource.tableId;
@@ -58,8 +60,8 @@
     try {
       await fetchCardsTable();
       API.fetchTableData(statusTableId).then(function (data){
-          tableStatuses = data; // set table status after looping of data
-          data.forEach((status) => {
+          tableStatuses = data;
+          tableStatuses.forEach((status) => {
             columns[status.Title] = [];
           });
           dataProvider.rows.forEach((card) => {
@@ -136,12 +138,16 @@
     <div class="header">
         <h2>{#if title}{title}{/if}</h2>
         <div class="header">
-          <input type="text" bind:value={searchQuery} placeholder="Search" class="customInput" />
+          <input type="text" bind:value={searchQuery} placeholder="Title & Desc" class="customInput" />
           <button
             on:click={modal.show()}
             class="spectrum-Button spectrum-Button--sizeM spectrum-Button--cta"
+            style="margin-right: 0.5rem;"
             >Add Column</button
           >
+          <button style="border: none; cursor: pointer;" on:click={orderModal.show()}>
+            <svg xmlns="http://www.w3.org/2000/svg" height="18" viewBox="0 0 18 18" width="18"><rect id="Canvas" fill="#ff13dc" opacity="0" width="18" height="18" /><path class="fill" d="M16.45,7.8965H14.8945a5.97644,5.97644,0,0,0-.921-2.2535L15.076,4.54a.55.55,0,0,0,.00219-.77781L15.076,3.76l-.8365-.836a.55.55,0,0,0-.77781-.00219L13.4595,2.924,12.357,4.0265a5.96235,5.96235,0,0,0-2.2535-.9205V1.55a.55.55,0,0,0-.55-.55H8.45a.55.55,0,0,0-.55.55V3.106a5.96235,5.96235,0,0,0-2.2535.9205l-1.1-1.1025a.55.55,0,0,0-.77781-.00219L3.7665,2.924,2.924,3.76a.55.55,0,0,0-.00219.77781L2.924,4.54,4.0265,5.643a5.97644,5.97644,0,0,0-.921,2.2535H1.55a.55.55,0,0,0-.55.55V9.55a.55.55,0,0,0,.55.55H3.1055a5.967,5.967,0,0,0,.921,2.2535L2.924,13.4595a.55.55,0,0,0-.00219.77782l.00219.00218.8365.8365a.55.55,0,0,0,.77781.00219L4.5405,15.076,5.643,13.9735a5.96235,5.96235,0,0,0,2.2535.9205V16.45a.55.55,0,0,0,.55.55H9.55a.55.55,0,0,0,.55-.55V14.894a5.96235,5.96235,0,0,0,2.2535-.9205L13.456,15.076a.55.55,0,0,0,.77782.00219L14.236,15.076l.8365-.8365a.55.55,0,0,0,.00219-.77781l-.00219-.00219L13.97,12.357a5.967,5.967,0,0,0,.921-2.2535H16.45a.55.55,0,0,0,.55-.55V8.45a.55.55,0,0,0-.54649-.55349ZM11.207,9A2.207,2.207,0,1,1,9,6.793H9A2.207,2.207,0,0,1,11.207,9Z" /></svg>
+          </button>
         </div>
     </div>
     <div
@@ -173,6 +179,13 @@
             </div>
           </Layout>
         </form>
+      </ModalContent>
+    </Modal>
+    <Modal bind:this={orderModal}>
+      <ModalContent title="Columns Positions" size="XL" onConfirm={addColumn}>
+        <SortColumns
+          {tableStatuses}
+        />
       </ModalContent>
     </Modal>
   {:else}
